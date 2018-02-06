24eac3ddbd2807aa20b410f14d5002f7f306a70a
Alexey Konovalenko <akonovalenko161@gmail.com>
2018-02-06 19:38:39 +0300

    Added support empty matrix

--- snail-sort/snail.php
+++ snail-sort/snail.php
@@ -9,38 +9,46 @@ function snail(array $array): array
 {
     $sortArr = [];
 
-    $i = 0;
-    $j = -1;
-    $colSteps = $count = count($array);
-    $rowSteps = $count - 1;
-    $elNum = pow($count, 2);
-    $vectors = [
-        ['ref' => &$j, 'step' => 1, 'limit' => &$colSteps],
-        ['ref' => &$i, 'step' => 1, 'limit' => &$rowSteps],
-        ['ref' => &$j, 'step' => -1, 'limit' => &$colSteps],
-        ['ref' => &$i, 'step' => -1, 'limit' => &$rowSteps],
-    ];
+    if ($colSteps = reset($array) ? count(reset($array)) : 0) {
+        $i = 0;
+        $j = -1;
+        $count = count($array);
+        $rowSteps = $count - 1;
+        $elNum = pow($count, 2);
+        $vectors = [
+            ['ref' => &$j, 'step' => 1, 'limit' => &$colSteps],
+            ['ref' => &$i, 'step' => 1, 'limit' => &$rowSteps],
+            ['ref' => &$j, 'step' => -1, 'limit' => &$colSteps],
+            ['ref' => &$i, 'step' => -1, 'limit' => &$rowSteps],
+        ];
 
-    while ($elNum) {
-        $vector = array_shift($vectors);
-        array_push($vectors, $vector);
-        $vector['ref'] += $vector['step'];
-        for ($l = 0; $l < $vector['limit']; ++$l) {
-            $sortArr[] = $array[$i][$j];
-            ($l + 1) !== $vector['limit'] ? $vector['ref'] += $vector['step'] : null;
-            --$elNum;
+        while ($elNum) {
+            $vector = array_shift($vectors);
+            array_push($vectors, $vector);
+            $vector['ref'] += $vector['step'];
+            for ($l = 0; $l < $vector['limit']; ++$l) {
+                $sortArr[] = $array[$i][$j];
+                ($l + 1) !== $vector['limit'] ? $vector['ref'] += $vector['step'] : null;
+                --$elNum;
+            }
+            --$vector['limit'];
         }
-        --$vector['limit'];
+    } else {
+        $sortArr = array_filter($array, function ($val) {
+            return ! (is_array($val) && empty($val));
+        });
     }
 
     return $sortArr;
 }
 
 $array = [
-    [1, 2, 3, 11],
-    [4, 5, 6, 12],
-    [7, 8, 9, 13],
-    [7, 8, 9, 14],
+    [1, 2, 3, 1],
+    [4, 5, 6, 4],
+    [7, 8, 9, 7],
+    [7, 8, 9, 7]
 ];
+
+$array2 = [[]];
 
 print_r(snail($array));
